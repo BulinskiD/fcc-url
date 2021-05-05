d46ab9174d0d788bb55b51ec76e56931b32f917f
Dawid Bulinski (PGS Software) <dbulinski@pgs-soft.com>
2021-05-05 15:59:59 +0200

    Fixed url name

--- server.js
+++ server.js
@@ -8,6 +8,11 @@ const app = express();
 
 const port = process.env.PORT || 3000;
 
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
 app.use(bodyParser.json());
 app.use(cors());
 
